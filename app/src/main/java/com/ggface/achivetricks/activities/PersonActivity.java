160f13af9c9ca0d68a556bd72f044afadea26dc1
ḠḠFACE <ggface@me.com>
2016-05-08 12:21:10 +0300

    мелочи

--- app/src/main/java/com/ggface/achivetricks/activities/PersonActivity.java
+++ app/src/main/java/com/ggface/achivetricks/activities/PersonActivity.java
@@ -1,7 +1,6 @@
 package com.ggface.achivetricks.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -32,35 +31,37 @@ public class PersonActivity extends AppCompatActivity {
                     .commit();
         }
 
-        if (Tools.containsString(getIntent().getExtras(), Units.ARG_JSON)){
+        if (Tools.containsString(getIntent().getExtras(), Units.ARG_JSON)) {
             String json = getIntent().getExtras().getString(Units.ARG_JSON);
             this.mPerson = new Gson().fromJson(json, Person.class);
             getSupportActionBar().setTitle(this.mPerson.name);
-        }else
+        } else if (Tools.containsInt(getIntent().getExtras(), Units.ARG_INDEX)) {
+            getSupportActionBar().setTitle("Edit lovely note");
+        } else
             getSupportActionBar().setTitle("New lovely note");
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_person, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_person, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
